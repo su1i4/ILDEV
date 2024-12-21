f4eae0f3579a3fb607e3866a062b3ff122d355db
Mac <mac@MacBook-Pro-som-tillhor-Mac.local>
2024-12-21 10:41:59 -0800

    save

--- src/screens/banner/index.tsx
+++ src/screens/banner/index.tsx
@@ -1,17 +1,21 @@
 import { Header } from "@/components/header";
 import { MarqueeUi } from "./ui/marquee";
 import { Middle } from "./ui/middle";
+import { Animation } from "./ui/animation";
 
 export const Banner = () => {
   return (
     <div className="w-full h-[100vh] relative overflow-hidden">
-      <div className="absolute top-8 w-full transition duration-300">
+       <div className="absolute top-0 bottom-0 w-full flex items-center justify-end">
+        <Animation />
+      </div>
+      <div className="absolute top-8 w-full">
         <Header />
       </div>
-      <div className="absolute top-0 bottom-0 w-full flex items-center transition duration-300">
+      <div className="absolute top-0 bottom-0 w-full flex items-center">
         <Middle />
       </div>
-      <div className="absolute bottom-0 w-full py-[20px] bg-lightDark transition duration-300">
+      <div className="absolute bottom-0 w-full py-[20px] bg-lightDark">
         <MarqueeUi />
       </div>
     </div>
---
+++ src/screens/banner/ui/animation/index.tsx
@@ -0,0 +1,9 @@
+import Spline from '@splinetool/react-spline';
+
+export const Animation = () => {
+  return (
+    <main style={{ width: '80%', height: '100vh' }}>
+      <Spline scene="https://prod.spline.design/pZiT4Co4SudKISUD/scene.splinecode" />
+    </main>
+  );
+};
