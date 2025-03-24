ff9a145ca11ab775d84699be4c9fb7467852ed1b
Samirkosh <samiramira1601@gmail.com>
2025-03-24 20:09:52 +0300

    adaptive lg

--- src/screens/main/ui/alpha-cargo/alpha-cargo-banner/index.tsx
+++ src/screens/main/ui/alpha-cargo/alpha-cargo-banner/index.tsx
@@ -28,14 +28,10 @@ const AlphaCargoBanner = () => {
   const t = useTranslations();
 
   return (
-    <div className=" bg-zinc-900 flex flex-col justify-between gap-[150px] px-6">
-      <div
-        className="relative flex lg:flex-col pt-20 lg:pt-32 px-4 lg:px-8 mt-[100px] sm:mt-0 
-       
-      "
-      >
+    <div className="bg-zinc-900 flex flex-col justify-between gap-[50px] px-6">
+      <div className="relative flex lg:flex-col pt-20 lg:pt-32 px-4 lg:px-8 mt-[100px] lg:gap-[88px]">
         <motion.div
-          className="container mx-auto lg:max-w-7xl z-10"
+          className="relative container mx-auto lg:max-w-7xl z-0"
           initial={fadeInUp.initial}
           animate={fadeInUp.animate}
           transition={fadeInUp.transition}
@@ -46,41 +42,39 @@ const AlphaCargoBanner = () => {
             </h1>
           </div>
         </motion.div>
-
         <motion.div
-          className="relative w-full sm:w-[35%] mt-12 lg:mt-0 lg:absolute lg:right-8 lg:top-1/2 lg:-translate-y-1/2"
+          className="relative z-0 w-full flex flex-col items-center"
           initial={fadeInRight.initial}
           animate={fadeInRight.animate}
           transition={fadeInRight.transition}
         >
-          <div className="relative flex justify-center lg:justify-start h-[400px] lg:h-[600px]">
-            <div className="w-[501px] h-[501px] sm:w-[321px] sm:h-[321px] rounded-full bg-custom-gradient absolute"></div>
+          <div className="w-[501px] h-[501px] rounded-full bg-custom-gradient"></div>
 
-            <motion.div
-              className="absolute left-0 lg:left-[120px] top-[100px]  z-[2] w-[610px]  md:w-[350px] md:top-[40px] lg:w-auto"
-              initial={imageAnimation.initial}
-              animate={imageAnimation.animate}
-              transition={imageAnimation.transition}
-            >
-              <Image
-                src={AlphaCargoImage1}
-                alt="Desktop view"
-                className="rounded-lg object-cover xl:w-[510px] shadow-lg hover:scale-105 transition-transform duration-300"
-              />
-            </motion.div>
-            <motion.div
-              className="absolute right-0 lg:left-[200px] md:left-[100px] lg:bottom-[200px] z-[1] w-[433px] md:bottom-[-30px] md:w-[300px] lg:w-auto"
-              initial={imageAnimation.initial}
-              animate={imageAnimation.animate}
-              transition={{ ...imageAnimation.transition, delay: 0.6 }}
-            >
-              <Image
-                src={AlphaCargoImage2}
-                alt="Mobile view"
-                className="rounded-lg shadow-xl object-cover hover:scale-105 transition-transform duration-300"
-              />
-            </motion.div>
-          </div>
+          <motion.div
+            className="w-[601px] md:w-[350px] lg:w-[601px] h-auto absolute top-[138px] z-[2]"
+            initial={imageAnimation.initial}
+            animate={imageAnimation.animate}
+            transition={imageAnimation.transition}
+          >
+            <Image
+              src={AlphaCargoImage1}
+              alt="Desktop view"
+              className="rounded-lg object-cover  hover:scale-105 transition-transform duration-300"
+            />
+          </motion.div>
+
+          <motion.div
+            className="w-[368px] md:w-[300px] lg:w-[368px] h-auto rotate-6 absolute top-[49px] right-[0px] z-[1]"
+            initial={imageAnimation.initial}
+            animate={imageAnimation.animate}
+            transition={{ ...imageAnimation.transition, delay: 0.6 }}
+          >
+            <Image
+              src={AlphaCargoImage2}
+              alt="Mobile view"
+              className="rounded-lg shadow-xl object-cover hover:scale-105 transition-transform duration-300"
+            />
+          </motion.div>
         </motion.div>
       </div>
 
@@ -91,7 +85,7 @@ const AlphaCargoBanner = () => {
         transition={{ duration: 0.6, delay: 0.8 }}
       >
         <div>
-          <div className="flex gap-[20px] md:flex-col sm:text-[15px] sm:text-normal">
+          <div className="flex lg:gap-32 gap-[20px]">
             <h2 className="text-2xl lg:text-3xl font-bold text-white text-nowrap">
               {t("alpha-cargo.project.about.title")}
             </h2>
